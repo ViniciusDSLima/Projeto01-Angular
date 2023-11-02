dc4d2349fd180c73c09c0b335236678e6eb85624
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-02 18:27:29 -0300

    Criado o pipe de icones

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { NgModule } from '@angular/core';
+import { CategoryPipe } from './pipe/category.pipe';
 
 @NgModule({
-  declarations: [ErrorDialogComponent],
+  declarations: [ErrorDialogComponent, CategoryPipe],
   imports: [CommonModule, AppMaterialModule],
-  exports: [ErrorDialogComponent],
+  exports: [ErrorDialogComponent, CategoryPipe],
 })
 export class SharedModule {}
