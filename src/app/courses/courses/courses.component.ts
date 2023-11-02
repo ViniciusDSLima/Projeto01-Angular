45419b1e97c2be8b6545697a3293d7a8c47b9316
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-02 13:17:34 -0300

    Utilizado o conceito de Ajax para pegar as rotas Http da requisicao da service.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
-import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -9,7 +10,7 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'FrontEnd' }];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private cousersService: CoursesService) {
