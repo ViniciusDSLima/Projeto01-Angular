b02af7c53420e2bcc32ea3764bf597f44feff379
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-11-02 12:23:45 -0300

    Criada  a tabela de cursos do Site

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'FrontEnd' }];
+  displayedColumns = ['name', 'category'];
 
+  constructor() {}
 }
